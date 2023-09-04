fe06af42349896eaa830c071b6734653a19a719e
Gloria Semprini <gloria.semprini@studio.unibo.it>
2023-09-04 12:34:24 +0200

    aggiunto cgan ma non finito

--- src/gan/gan.py
+++ src/gan/gan.py
@@ -13,18 +13,20 @@ import utils.gan_utils as gu1
 
 
 class Gan:
-  def build_gan(shape,neuron_count_per_hidden_layer,output_dim,hidden_activation,generator_output_activation):
+  def build_gan(self,input_noise_dim,neuron_count_per_hidden_layer,image_shape,n_pixel,hidden_activation,generator_output_activation):
     #Generator
     generator = keras.Sequential(name='generator')
-    generator.add(layers.Input(shape=shape, name='generator_input'))
+    generator.add(layers.Input(shape=input_noise_dim, name='generator_input'))
     for neuron_count in neuron_count_per_hidden_layer:
       generator.add(layers.Dense(neuron_count, activation=hidden_activation))
     
-    generator.add(layers.Dense(output_dim, activation=generator_output_activation,name='generator_output'))
+    generator.add(layers.Dense(n_pixel, activation=generator_output_activation,name='generator_output'))
+    generator.add(layers.Reshape(image_shape))
     
     #Discriminator
     discriminator = keras.Sequential(name='discriminator')
-    discriminator.add(layers.Input(shape=output_dim,name='discriminator_input'))
+    discriminator.add(layers.Input(shape=image_shape,name='discriminator_input'))
+    discriminator.add(layers.Reshape((n_pixel,)))
     for neuron_count in reversed(neuron_count_per_hidden_layer):
       discriminator.add(layers.Dense(neuron_count, activation=hidden_activation))
     
@@ -34,7 +36,7 @@ class Gan:
     gan = keras.Model(generator.input, discriminator(generator.output),name='gan')
     return gan,generator,discriminator
   
-  def train_gan(gan,generator,discriminator,train_x,train_data_count,input_noise_dim,epoch_count, batch_size,
+  def train_gan(self,gan,generator,discriminator,train_generator,train_data_count,input_noise_dim,epoch_count, batch_size,
               get_random_input_func,get_real_batch_func,get_fake_batch_func,concatenate_batches_func,condition_count=-1,
               use_one_sided_labels=False,plt_frq=None,plt_example_count=10,example_shape=(28,28)):
     iteration_count = int(train_data_count / batch_size)
@@ -49,7 +51,8 @@ class Gan:
       print('Before training:')
       noise_to_plot = get_random_input_func(plt_example_count, input_noise_dim,condition_count)
       generated_output = generator.predict(noise_to_plot,verbose=0)
-      generated_images = generated_output.reshape(plt_example_count, example_shape[0], example_shape[1])
+      #generated_images = generated_output.reshape(plt_example_count, example_shape[0], example_shape[1])
+      generated_images = generated_output
       ploters.plot_generated_images([generated_images],1,plt_example_count,figsize=(15, 5))
           
     d_epoch_losses=[]
@@ -66,8 +69,9 @@ class Gan:
             current_batch_size=len(batch_indices[i])
 
             # 1. create a batch with real images from the training set
-            real_batch_x,real_batch_y=get_real_batch_func(train_x,batch_indices[i],0.9 if use_one_sided_labels else 1)
-                        
+            real_batch_x,real_batch_y=get_real_batch_func(train_generator,batch_indices[i],0.9 if use_one_sided_labels else 1)
+            #real_batch_x=get_real_batch_func(train_generator,batch_indices[i],0.9 if use_one_sided_labels else 1)
+
             # 2. create noise vectors for the generator and generate the images from the noise
             generator_input=get_random_input_func(current_batch_size, input_noise_dim,condition_count)
             fake_batch_x,fake_batch_y=get_fake_batch_func(generator,current_batch_size,generator_input)
--- src/main_gan.py
+++ src/main_gan.py
@@ -8,6 +8,9 @@ import tensorflow as tf
 from tensorflow import keras
 from numba import cuda
 import importlib
+from keras import layers
+
+import graphviz
 
 import utils.paths as paths
 import utils.callbacks as callbacks
@@ -16,6 +19,7 @@ import utils.ploters as ploters
 import utils.image_generator as im_gen
 
 import gan.gan as g1
+import gan.cgan as cg1
 import utils.gan_utils as g_ut
 
 # %%
@@ -23,10 +27,12 @@ importlib.reload(im_gen)
 importlib.reload(paths)
 importlib.reload(ploters)
 importlib.reload(g1)
+importlib.reload(cg1)
+importlib.reload(g_ut)
 # %% Generator
 importlib.reload(im_gen)
-image_heigh = 80
-image_weigh = 80
+image_heigh = 64
+image_weigh = 64
 NUM_COLORS = 1
 BATCH_SIZE = 64
 imageSize = (image_heigh, image_weigh)
@@ -42,28 +48,27 @@ train_generator, validation_generator = im_gen.createImageGenerator(
 
 im_gen.plotGeneratedImages(train_generator)
 
-# %% GAN
+# %% GAN - Model creation
 importlib.reload(g1)
+importlib.reload(g_ut)
 
-
-# %% Model creation
 input_noise_dim=100
 NUM_PIXELS = image_heigh * image_weigh * NUM_COLORS
 image_shape = (image_heigh, image_weigh, NUM_COLORS)
 arr = [256,512,1024]
 
 gan, gan_generator, gan_discriminator = g1.Gan().build_gan(
-  input_noise_dim,
-  arr,
-  image_shape,
-  'relu',
-  'sigmoid')
+                                        input_noise_dim,
+                                        arr,
+                                        image_shape,
+                                        NUM_PIXELS,
+                                        'relu',
+                                        'sigmoid')
 
-# %% Summary
-gan.summary()
-keras.utils.plot_model(gan,show_shapes=True, show_layer_names=True,expand_nested=True)
+gan_generator.summary()
+gan_discriminator.summary()
+g_ut.plotGAN(gan)
 
-# %% Model compilation
 gan_discriminator.compile(loss='binary_crossentropy', optimizer='sgd')
 
 gan_discriminator.trainable = False
@@ -73,14 +78,14 @@ gan.compile(loss='binary_crossentropy', optimizer='sgd')
 
 
 # %% Execute training
-epoch_count=10
+epoch_count=1000
 batch_size=100
 
-d_epoch_losses,g_epoch_losses=g1.train_gan(gan,
+d_epoch_losses,g_epoch_losses=g1.Gan().train_gan(gan,
                                         gan_generator,
                                         gan_discriminator,
-                                        g_ut.val_x_flatten,
-                                        g_ut.val_x_flatten.shape[0],
+                                        train_generator,
+                                        2000, # TODO numero di immagini
                                         input_noise_dim,
                                         epoch_count,
                                         batch_size,
@@ -88,47 +93,53 @@ d_epoch_losses,g_epoch_losses=g1.train_gan(gan,
                                         g_ut.get_gan_real_batch,
                                         g_ut.get_gan_fake_batch,
                                         g_ut.concatenate_gan_batches,
-                                        plt_frq=1,
-                                        plt_example_count=15)
+                                        plt_frq=20,
+                                        plt_example_count=15,
+                                        example_shape=image_shape)
 
 ploters.plot_gan_losses(d_epoch_losses,g_epoch_losses)
 
-# %% Tips and tricks for training
-train_x_flatten = (train_x_flatten*2)-1
-val_x_flatten = (val_x_flatten*2)-1
-test_x_flatten = (test_x_flatten*2)-1
+# %% Tips prof, ma il normalize input non serve, no?
+importlib.reload(g1)
+importlib.reload(g_ut)
 
 generator_output_activation='tanh'
-
-# $$ Avoid sparse gradients
-hidden_activation=keras.layers.LeakyReLU(alpha=0.2)
-
-# %% One-sided label smoothing
 use_one_sided_labels=True
-
-# %% Use Adam optimizer
 optimizer = keras.optimizers.legacy.Adam(learning_rate=0.0002, beta_1=0.5)
 
-# %% Execute training
-epoch_count=30
-batch_size=100
+NUM_PIXELS = image_heigh * image_weigh * NUM_COLORS
+image_shape = (image_heigh, image_weigh, NUM_COLORS)
+arr = [256,512,1024]
 
-gan,gan_generator,gan_discriminator=g1.build_gan(input_noise_dim,
-                                              [256,512,1024],
-                                              train_x_flatten.shape[1],
+hidden_activation=layers.LeakyReLU(alpha=0.2)
+
+gan,gan_generator,gan_discriminator=g1.Gan().build_gan(input_noise_dim,
+                                              arr,
+                                              image_shape,
+                                              NUM_PIXELS,
                                               hidden_activation,
                                               generator_output_activation)
+
+
+gan_generator.summary()
+gan_discriminator.summary()
+g_ut.plotGAN(gan)
+
 
 gan_discriminator.compile(loss='binary_crossentropy', optimizer=optimizer)
 
 gan_discriminator.trainable = False
 gan.compile(loss='binary_crossentropy', optimizer=optimizer)
 
-d_epoch_losses,g_epoch_losses=g1.train_gan(gan,
+# %%
+epoch_count=1000
+batch_size=200
+
+d_epoch_losses,g_epoch_losses=g1.Gan().train_gan(gan,
                                         gan_generator,
                                         gan_discriminator,
-                                        val_x_flatten,
-                                        val_x_flatten.shape[0],
+                                        train_generator,
+                                        2000, # TODO numero di immagini
                                         input_noise_dim,
                                         epoch_count,
                                         batch_size,
@@ -137,28 +148,31 @@ d_epoch_losses,g_epoch_losses=g1.train_gan(gan,
                                         g_ut.get_gan_fake_batch,
                                         g_ut.concatenate_gan_batches,
                                         use_one_sided_labels=use_one_sided_labels,
-                                        plt_frq=1,
-                                        plt_example_count=15)
+                                        plt_frq=10,
+                                        plt_example_count=15,
+                                        example_shape=image_shape)
 
 ploters.plot_gan_losses(d_epoch_losses,g_epoch_losses)
 
-# %% Generated images
-noise = np.random.normal(0, 1, size=(1, input_noise_dim))
+# %%
+importlib.reload(cg1)
+importlib.reload(g_ut)
 
-generated_x = gan_generator.predict(noise,verbose=0)
-digit = generated_x[0].reshape(original_image_shape)
+input_noise_dim=100
+hidden_activation=layers.LeakyReLU(alpha=0.2)
+generator_output_activation='tanh'
 
-plt.axis('off')
-plt.imshow(digit, cmap='gray')
-plt.show()
+NUM_PIXELS = image_heigh * image_weigh * NUM_COLORS
+image_shape = (image_heigh, image_weigh, NUM_COLORS)
+arr = [256,512,1024]
+#input, cond, arr, shape, n_pixel, hidd,gen
+cgan,cgan_generator,cgan_discriminator=cg1.cGAN().build_cgan(input_noise_dim,
+                                                             cond,
+                                                             arr,
+                                                             image_shape,
+                                                             NUM_PIXELS,
+                                                             hidden_activation,
+                                                             generator_output_activation)
 
-# %% RUN
-n = 10 # number of images per row and column
-
-generated_images=[]
-for i in range(n):
-  noise = np.random.normal(0, 1, size=(n, input_noise_dim))
-  generated_x = gan_generator.predict(noise,verbose=0)
-  generated_images.append([g.reshape(original_image_shape) for g in generated_x])
-
-ploters.plot_generated_images(generated_images,n,n)
+cgan.summary()
+g_ut.plotGAN(cgan)
