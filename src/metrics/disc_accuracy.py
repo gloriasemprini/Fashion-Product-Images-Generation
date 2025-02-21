0a2b2b634d56bca4c75a15a511d164e6d455e779
Gloria Semprini <gloria.semprini@studio.unibo.it>
2025-02-21 18:12:35 +0100

    add disc_accuracy

--- src/metrics/disc_accuracy.py
+++ src/metrics/disc_accuracy.py
@@ -1,19 +1,21 @@
 from keras.metrics import BinaryAccuracy
 import numpy as np
 
-# Crea un oggetto BinaryAccuracy
-accuracy = BinaryAccuracy()
+def calculate_discriminator_accuracy(real_images, real_labels, fake_images, fake_labels, discriminator):
+    # Creiamo due array di etichette corrette: 1 per reali, 0 per fake
+    real_targets = np.ones((real_images.shape[0], 1))
+    fake_targets = np.zeros((fake_images.shape[0], 1))
 
-def calculate_discriminator_accuracy(real_images, fake_images, discriminator):
-    # Classifica le immagini reali e false
-    real_labels = np.ones((real_images.shape[0], 1))
-    fake_labels = np.zeros((fake_images.shape[0], 1))
-    
-    real_preds = discriminator.predict(real_images)
-    fake_preds = discriminator.predict(fake_images)
-    
-    # Calcola l'accuratezza per le immagini reali e false
-    accuracy_real = accuracy.update_state(real_labels, real_preds)
-    accuracy_fake = accuracy.update_state(fake_labels, fake_preds)
-    
+    # Passiamo sia immagini che etichette al discriminatore
+    real_preds = discriminator.predict([real_images, real_labels])
+    fake_preds = discriminator.predict([fake_images, fake_labels])
+
+    # Convertiamo le probabilità in valori binari (0 o 1)
+    real_preds = (real_preds > 0.5).astype(np.float32)
+    fake_preds = (fake_preds > 0.5).astype(np.float32)
+
+    # Calcoliamo l'accuratezza separata per reali e fake
+    accuracy_real = BinaryAccuracy()(real_targets, real_preds).numpy()
+    accuracy_fake = BinaryAccuracy()(fake_targets, fake_preds).numpy()
+
     return accuracy_real, accuracy_fake
